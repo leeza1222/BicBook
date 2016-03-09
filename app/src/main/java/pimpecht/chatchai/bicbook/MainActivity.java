ded32febfc3a0235d34e1c660b8fbb0079ed2701
leeza1222@gmail.com <bank030347941>
2016-03-09 10:25:24 +0700

    Intent to SignUp Finis

--- app/src/main/java/pimpecht/chatchai/bicbook/MainActivity.java
+++ app/src/main/java/pimpecht/chatchai/bicbook/MainActivity.java
@@ -1,7 +1,9 @@
 package pimpecht.chatchai.bicbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }// mmain Method
+
+    public void clickSignUpMain(View View) {
+        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+
     }
-}
+
+}// Main Class
